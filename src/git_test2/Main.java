5baf6a48edea855c11a79b6b4d2d97de74e99f76
Han, Sangyoen <coyomy@naver.com>
2020-09-15 22:12:23 +0900

    Main 함수 수정!

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
         Hello h = new Hello();
         h.say();
-        int i=0;
-        String j="안녕!";
-        //ㅇㄹㄹㅇㄹㅇ
+
     }
 
 
