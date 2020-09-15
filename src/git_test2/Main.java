01c2e37bfd96472fb9897c70a0975c2b405773c4
Han, Sangyoen <coyomy@naver.com>
2020-09-15 22:09:58 +0900

    Main 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -2,8 +2,13 @@ package git_test2;
 
 public class Main {
 
-    public static void main(String[] args) {
-        new Hello().say();
+	public static void main(String[] args) {
+        Hello h = new Hello();
+        h.say();
+        int i=0;
+        String j="안녕!";
+        //ㅇㄹㄹㅇㄹㅇ
     }
+
 
 }
